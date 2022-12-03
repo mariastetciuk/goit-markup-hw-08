c957467566e4043bc4f2a5c3a2e58d6f5107f99a
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2022-12-03 02:10:57 +0200

    адаптивна верстка

--- modal.js
+++ modal.js
@@ -9,6 +9,8 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle("modal-open");
     refs.modal.classList.toggle("backdrop--is-hidden");
   }
+  
 })();
